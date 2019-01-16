aec43420f18adbd091259821912fb74baa50c5fc
binsom <18300163780@163.com>
2019-01-16 10:25:54 +0800

    first commit为引入vux

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,27 @@
 
     <img src="./assets/logo.png">
     <router-view/>
+    <tabbar>
+      <tabbar-item>
+        <span slot="label">待办提醒</span>
+      </tabbar-item>
+      <tabbar-item>
+        <span slot="label">项目信息查询</span>
+      </tabbar-item>
+    </tabbar>
   </div>
 </template>
 
 <script>
   import { XHeader, Tabbar, TabbarItem } from 'vux';
-export default {
-  components: {
-    XHeader,
-    Tabbar,
-    TabbarItem
-  },
-  name: 'App'
-}
+  export default {
+    components: {
+      XHeader,
+      Tabbar,
+      TabbarItem
+    },
+    name: 'App'
+  }
 </script>
 
 <style>
